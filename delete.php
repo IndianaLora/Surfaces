dc1d058cec283f5ac9437ea13a5fc6c985f9ccee
Indiana Lora <indiana@ensaludybelleza.com>
2020-08-03 10:29:46 -0400

    Update delete.php

--- delete.php
+++ delete.php
@@ -3,13 +3,15 @@ require_once '../Surface/user/usuario.php';
 require_once '../Surface/database/fileHandler/registroContext.php';
 require_once '../Surface/logica/metodos.php';
 require_once '../Surface/logica/Rdatabase.php';
+require_once '../Surface/publicaciones/Pdatabase.php';
+require_once '../Surface/publicaciones/pensamiento.php';
 
-
-$service = new Rdatabase("./database");
-$isContaintId= isset($_GET['id']);
+$pensamiento = new pensamiento();
+$service = new Pdatabase("./database");
+$isContaintId= isset($_GET['eliminarId']);
 
 if($isContaintId){
-    $Id=$_GET['id'];
+    $Id=$_GET['eliminarId'];
     $service->Delete($Id);
    }
 
