ab56f21a1303e088498cb4f37c2f3ced12f2cb1b
Indiana Lora <indiana@ensaludybelleza.com>
2020-08-03 12:28:20 -0400

    Update editar.php

--- editar.php
+++ editar.php
@@ -4,122 +4,75 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Registros</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
   <link rel="stylesheet" href="css/style.css">
+  <title>Document</title>
 </head>
 
-<body class="editar">
+<body class="pokelist">
   <?php
-  //incluyo BD ,usuarioes ya que voy a crear uno nuevo y los metodos para poder utilizarlos
-  include '../database/boots/header.php';
+  //////////////////si reinicio la pagina se repite el ultimo array en la lista de arrays
+  include('../Surface/boots/sheader.php');
+
   require_once '../Surface/database/fileHandler/registroContext.php';
-   require_once '../Surface/logica/metodos.php';
-   require_once '../Surface/logica/Rdatabase.php';
-   require_once '../Surface/logica/usuario.php';
+  require_once '../Surface/logica/metodos.php';
+  require_once '../Surface/logica/Rdatabase.php';
+  require_once '../Surface/publicaciones/Pdatabase.php';
+  require_once '../Surface/user/uService.php';
+  require_once '../Surface/user/usuario.php';
+  require_once '../Surface/publicaciones/pensamiento.php';
 
-  // un objeto de nombre $codigo y $service que contenga las variables y metodos que necesito
-  $codigo = new metodos();
-  $service = new Rdatabase("./database");
+  session_start();
+ 
+  $services = new Pdatabase("../database");
+  $listPublicaciones = $services->GetList();
+  $newPensamiento = new pensamiento();
+ 
+  $idUsuario=$_SESSION['usuario']->id;
+  $nombre = $_SESSION['usuario']->usuario;
+  $foto = $_SESSION['usuario']->foto;
+  $tiempo = "porfin";
 
-  //si el id esta seteado almacena ese valor en $pokeId y se lo paso por parametro a GetbyId($pokeId);
-  if (isset($_GET['id'])) {
-    $pokeId = $_GET['id'];
-    //GetbyId se utilza para obtener un obejeto en especifico por su ID
-    $element = $service->GetbyId($pokeId);
+  foreach ($listPublicaciones as $value) {
+      if ($idUsuario == $value->idUsuario) {
+          $titulo = $value->titulo;
+          $pensamiento = $value->pensamiento;
+          $idUsuario = $value->idUsuario;
+      }
 
-    if (isset($_POST["nombre"]) && isset($_POST["region"]) && isset($_POST["tipo"])) { //Verificamos que todos los elementos esten
 
-      $usuario = new usuario(); // creamos un nuevo objeto o usuario 
-
-      $usuario->InicializeData($pokeId, $_POST["nombre"], $_FILES["pokeFoto"], $_POST["region"], $_POST["tipo"], $_POST["Stipo"], $_POST["color"]); //seteamos los valores
-
-      $service->Update($pokeId, $usuario); //Lo actualizamos con el metodo previamente hecho update
-
-      header("location:/pokedex/pokeList.php"); //redireccionamos la pagina a la lista de usuarioes
-    }
+      if (isset($_POST['titulo']) && isset($_POST['pensamiento'])) {
+          $newPensamiento->initialize($idUsuario,$_POST['titulo'], $_POST['pensamiento'],$tiempo);
+          $services->Update($idUsuario, $newPensamiento);
+          header('Location:surPage.php');
+          exit();
+      }
   }
-
+  var_dump($newPensamiento);
   ?>
-  <div>
-    <br>
-    <br>
-    <div class="formulario-editar">
-      <br>
-      <br>
-      <br>
-      <h1 class="text-center">Poke Edicion</h1>
-      <!--Get Post redirect-->
-      <!-- especificamo el usuario que se va a editar atraves del id que tiene -->
-
-      <form action="pokeEditar.php?id=<?php echo $element->id ?>" enctype="multipart/form-data" method="POST" name="pokeEditar">
-        <!--POST se utiliza para los form siempre -->
-        </br>
-        <table align="center">
-          <th><label for="">Nombre</label>
-          <td><input type="text" name="nombre" class="form-control" value="<?php echo $element->nombre; ?>"></td>
-          <tr>
-
-          <tr>
-          <tr>
-          <tr>
-            <th><label for="photo">PokeFoto</label>
-            <td><input type="file" name="pokeFoto" class="form-control" value="<?php echo $element->pokeFoto; ?>" rows="3"></td>
-          <tr>
-
-            <th><label for="">Region</label>
-            <td><select name="region">
-                <?php foreach ($codigo->region as $id => $text) : ?>
-                  <?php if ($id == $element->region) : ?>
-                    <option selected value="<?php echo $text ?>"><?php echo "{$text}" ?></option>
-                  <?php else : ?>
-                    <option value="<?php echo $text ?>"><?php echo "{$text}" ?></option>
-                  <?php endif ?>
-
-                <?php endforeach; ?>
-
-              </select>
-            <th><label for="">Tipos</label>
-            <td><select name="tipo">
-                <?php foreach ($codigo->tipo as $id => $text) : ?>
-                  <?php if ($id == $element->tipo) : ?>
-                    <option selected value="<?php echo $text ?>"><?php echo "{$text}" ?></option>
-                  <?php else : ?>
-                    <option value="<?php echo $text ?>"><?php echo "{$text}" ?></option>
-                  <?php endif ?>
-
-                <?php endforeach; ?>
-
-              </select>
-            </td>
-          <tr>
-            <th>
-            <th><label for="">Segundo Tipos</label>
-            <td><select name="Stipo">
-                <?php foreach ($codigo->Stipo as $id => $text) : ?>
-                  <?php if ($id == $element->Stipo) : ?>
-                    <option selected value="<?php echo $text ?>"><?php echo "{$text}" ?></option>
-                  <?php else : ?>
-                    <option value="<?php echo $text ?>"><?php echo "{$text}" ?></option>
-                  <?php endif ?>
-
-                <?php endforeach; ?>
-
-              </select>
-            </td>
-          <tr>
-            <th>
-            <th><label for="">Color</label>
-            <td><input type="color" class="form-control" name="color" value="<?php echo $element->color; ?>"></td>
-            </th>
-            <th>
-            </th>
-            <button type="submit" name="submit" class="btn btn-info float-right" onclick="pokeList.php">Guardar</button>
-
-        </table>
-      </form>
+  
+  <form method="POST" action="editar.php">
+    
+    <div class="form-group">
+     Titulo <input name="titulo" value="<?php echo $value->titulo;?>">
+      <h2>Comparte lo que piensas<h2>
+          <textarea class="form-control" name="pensamiento" rows="5"><?php echo $value->pensamiento;?></textarea>
     </div>
+    <button type="submit" class="btn btn-success">Guardar</button>
+    </table>
+
+  </form>
+
+  <br>
+  <br>
+ 
+    
   </div>
+  </div>
+
 </body>
+<?php
+include('../Surface/boots/footer.php');
+?>
 
 </html>
